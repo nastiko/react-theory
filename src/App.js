7b90847c5b06bc735c4a76c2713399408cebc8f2
Anastasia H. <anastasia.hrynkevich@outlook.com>
2024-04-22 22:55:26 +0100

    set up tailwind

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 
 function App() {
-  return (
-   <div>
-     text
-   </div>
-  );
+    return (
+        <div>
+            <h1 className="text-3xl font-bold underline">Text 1</h1>
+            <h2>Text 2</h2>
+        </div>
+    );
 }
 
 export default App;
