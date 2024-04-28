5d137ac6aed48e09ed632d76d9d5c7b20ebefc19
Anastasia H. <anastasia.hrynkevich@outlook.com>
2024-04-28 20:18:17 +0100

    practise: organised code to use const

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import React from 'react';
 
 function App() {
     const arr = [1, 2, 3, 4, 5];
+    const list = <>{arr.map((item) => <li>List: {item}</li>)}</>
 
     return (
         <>
             <ul>
-                {arr.map((item) => <li>List: {item}</li>)}
+                {list}
             </ul>
 
         </>
