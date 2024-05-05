adaaaf7a05b24c886ba8d10d6bd989ec43f9c511
Anastasia H. <anastasia.hrynkevich@outlook.com>
2024-05-05 20:53:07 +0100

    practise: worked with object

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import React from 'react';
 
 function App() {
+    const users = [
+        {name: 'user1', surn: 'surn1', age: 30},
+        {name: 'user2', surn: 'surn2', age: 31},
+        {name: 'user3', surn: 'surn3', age: 32},
+    ];
 
-    function nameUser(name) {
-        alert('Hello, ' + name);
-    }
+    const result = users.map((user, index) => {
+        return <li> key={index} {user.name} {user.surn} - {user.age} </li>
+    })
+
+
 
     return (
         <>
-            <button onClick={() => nameUser('Anastasia')}>Click me!</button>
+            <div>{result}</div>
         </>
     );
 }
