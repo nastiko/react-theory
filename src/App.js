a82bae201d1aa8993c42f5c79c013de5bb05d790
Anastasia H. <anastasia.hrynkevich@outlook.com>
2024-05-05 21:00:02 +0100

    practise: created table

--- src/App.js
+++ src/App.js
@@ -2,20 +2,35 @@ import React from 'react';
 
 function App() {
     const users = [
-        {name: 'user1', surn: 'surn1', age: 30},
-        {name: 'user2', surn: 'surn2', age: 31},
-        {name: 'user3', surn: 'surn3', age: 32},
+        {id: 1, name: 'user1', surname: 'surn1', age: 30},
+        {id: 2, name: 'user2', surname: 'surn2', age: 31},
+        {id: 3, name: 'user3', surname: 'surn3', age: 32},
     ];
 
-    const result = users.map((user, index) => {
-        return <li> key={index} {user.name} {user.surn} - {user.age} </li>
+    const result = users.map((user) => {
+        return <tr key={user.id}>
+            <td>{user.name}</td>
+            <td>{user.surname}</td>
+            <td>{user.age}</td>
+        </tr>
     })
 
 
 
     return (
         <>
-            <div>{result}</div>
+            <table>
+                <thead>
+                <tr>
+                    <td>Name</td>
+                    <td>Surname</td>
+                    <td>Age</td>
+                </tr>
+                </thead>
+                <tbody>
+                {result}
+                </tbody>
+            </table>
         </>
     );
 }
