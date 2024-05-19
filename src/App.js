7534d7dca49948815c3f8b67de225a52ade3ac1a
Anastasia H. <anastasia.hrynkevich@outlook.com>
2024-05-19 10:46:31 +0100

    practise: conversion F in C

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import React, {useState} from 'react';
 
+function conversion(num) {
+    return ((num - 32)/1.8).toString();
+}
+
 function App() {
-    const [year, setBornYear] = useState('');
+    const [fahrenheit, setFahrenheit] = useState(0);
 
     function handleChange (event) {
-        const today = new Date();
-
-        setBornYear((today.getFullYear() - event.target.value).toString());
+        return setFahrenheit(event.target.value);
     }
 
     return (
         <>
-            <input className="border-[1px] border-[#000000]" type="number" onChange={handleChange}/>
-            <p>When user was born: {year}</p>
+            <input className="border-[1px] border-[#000000]" type="number" value={fahrenheit} onChange={handleChange}/>
+            <p>Conversion F in C: {conversion(fahrenheit)}</p>
         </>
     );
 }
