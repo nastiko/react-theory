fa61d6081850fd733147d31ace31c52c9e526e5a
Anastasia H. <anastasia.hrynkevich@outlook.com>
2024-05-19 11:41:17 +0100

    practise: processing form data on button click in React - fund sum & multiply

--- src/App.js
+++ src/App.js
@@ -3,28 +3,24 @@ import React, {useState} from 'react';
 function App() {
     const [value1, setValue1] = useState('');
     const [value2, setValue2] = useState('');
-    const [value3, setValue3] = useState('');
-    const [value4, setValue4] = useState('');
-    const [value5, setValue5] = useState('');
+
+    const [sum, setSum] = useState(0);
+    const [multiply, setMultiply] = useState(0);
 
     function handleChange1 (event) {
-        return setValue1(+event.target.value); //`+` operator converts the string to a number
+        return setValue1(event.target.value);
     }
 
     function handleChange2 (event) {
-        return setValue2(+event.target.value);
+        return setValue2(event.target.value);
     }
 
-    function handleChange3 (event) {
-        return setValue3(+event.target.value);
+    function handleSum() {
+        return setSum(Number(value1) + Number(value2));
     }
 
-    function handleChange4 (event) {
-        return setValue4(+event.target.value);
-    }
-
-    function handleChange5 (event) {
-        return setValue5(+event.target.value);
+    function handleMultiply() {
+        return setMultiply(Number(value1) * Number(value2));
     }
 
     return (
@@ -32,11 +28,15 @@ function App() {
             <div className="flex gap-2">
                 <input className="border-[1px] border-[#000000]" type="number" onChange={handleChange1}/>
                 <input className="border-[1px] border-[#000000]" type="number" onChange={handleChange2}/>
-                <input className="border-[1px] border-[#000000]" type="number" onChange={handleChange3}/>
-                <input className="border-[1px] border-[#000000]" type="number" onChange={handleChange4}/>
-                <input className="border-[1px] border-[#000000]" type="number" onChange={handleChange5}/>
             </div>
-            <p>Find average: {(value1 + value2 + value3 + value4 + value5) / 5}</p>
+            <div className="flex gap-2 mt-6">
+                <p>Find sum: {sum}</p>
+                <p>Find multiply: {multiply}</p>
+            </div>
+            <div className="flex gap-2 mt-6">
+                <button onClick={handleSum} className="bg-orange-500 rounded px-[15px] py-[4px] ">Sum</button>
+                <button onClick={handleMultiply} className="bg-amber-400 rounded px-[15px] py-[4px] ">Multiply</button>
+            </div>
         </>
     );
 }
