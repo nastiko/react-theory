c9a522e96c5772ff4b7f66bcfbaea9e68cdb4dc4
Anastasia H. <anastasia.hrynkevich@outlook.com>
2024-05-19 11:08:24 +0100

    practise: find average and use `+` operator to convert the string to a number

--- src/App.js
+++ src/App.js
@@ -1,20 +1,42 @@
 import React, {useState} from 'react';
 
-function conversion(num) {
-    return ((num - 32)/1.8).toString();
-}
-
 function App() {
-    const [fahrenheit, setFahrenheit] = useState(0);
+    const [value1, setValue1] = useState('');
+    const [value2, setValue2] = useState('');
+    const [value3, setValue3] = useState('');
+    const [value4, setValue4] = useState('');
+    const [value5, setValue5] = useState('');
 
-    function handleChange (event) {
-        return setFahrenheit(event.target.value);
+    function handleChange1 (event) {
+        return setValue1(+event.target.value); //`+` operator converts the string to a number
+    }
+
+    function handleChange2 (event) {
+        return setValue2(+event.target.value);
+    }
+
+    function handleChange3 (event) {
+        return setValue3(+event.target.value);
+    }
+
+    function handleChange4 (event) {
+        return setValue4(+event.target.value);
+    }
+
+    function handleChange5 (event) {
+        return setValue5(+event.target.value);
     }
 
     return (
         <>
-            <input className="border-[1px] border-[#000000]" type="number" value={fahrenheit} onChange={handleChange}/>
-            <p>Conversion F in C: {conversion(fahrenheit)}</p>
+            <div className="flex gap-2">
+                <input className="border-[1px] border-[#000000]" type="number" onChange={handleChange1}/>
+                <input className="border-[1px] border-[#000000]" type="number" onChange={handleChange2}/>
+                <input className="border-[1px] border-[#000000]" type="number" onChange={handleChange3}/>
+                <input className="border-[1px] border-[#000000]" type="number" onChange={handleChange4}/>
+                <input className="border-[1px] border-[#000000]" type="number" onChange={handleChange5}/>
+            </div>
+            <p>Find average: {(value1 + value2 + value3 + value4 + value5) / 5}</p>
         </>
     );
 }
