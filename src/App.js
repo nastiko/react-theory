f01a92f07ab13b5fbc6b22152bc381610be974cf
Anastasia H. <anastasia.hrynkevich@outlook.com>
2024-05-19 12:54:55 +0100

    practise: displaying a select box with options

--- src/App.js
+++ src/App.js
@@ -1,34 +1,23 @@
 import React, {useState} from 'react';
 
 function App() {
-    let message;
-    const [checked, setChecked1] = useState(false);
+    const [value, setValue] = useState('');
 
-    function handleChange() {
-        setChecked1(!checked);
-    }
-
-    if (checked) {
-        message =
-            <div>
-                <h2>Woo-Hoo, you're 18!</h2>
-            </div>;
-    } else {
-        message =
-            <div>
-                <h2>Sorry, content only for 18+</h2>
-            </div>;
+    function handleChange(event) {
+        setValue(event.target.value)
     }
 
     return (
         <>
             <div className="flex flex-col justify-center items-center gap-2 my-10">
-                <p>Are you 18 years old?</p>
-                <div>
-                    <input className="border-[1px] border-[#000000]" type="checkbox" checked={checked} onChange={handleChange}/>
-                    <span className="ml-2">Yes</span>
-                </div>
-                <div>{checked ? message : ''}</div>
+                <lavel>Choose a fruit:
+                    <select value={value} onChange={handleChange}>
+                        <option>Apple</option>
+                        <option>Banana</option>
+                        <option>Orange</option>
+                    </select>
+                </lavel>
+                <p>User has chosen: {value}</p>
             </div>
         </>
     );
