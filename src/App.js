1f6c15fc61fe29de6058899d42e72e1f434f5be9
Anastasia H. <anastasia.hrynkevich@outlook.com>
2024-05-19 12:24:38 +0100

    practise: working with checkbox - ex2

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
 import React, {useState} from 'react';
 
 function App() {
-    const [checked, setChecked] = useState(true);
-    const [response, setResponse] = useState(true);
+    const [checked1, setChecked1] = useState(false);
+    const [checked2, setChecked2] = useState(false);
+    const [checked3, setChecked3] = useState(false);
 
-    function handleChange() {
-        setChecked(!checked);
+    function handleChange1() {
+        setChecked1(!checked1);
     }
 
-    function handleResponse() {
-        setResponse(checked);
+    function handleChange2() {
+        setChecked2(!checked2);
+    }
+
+    function handleChange3() {
+        setChecked3(!checked3);
     }
 
     return (
         <>
-            <div className="flex justify-center items-center gap-2 my-10">
-                <input className="border-[1px] border-[#000000]" type="checkbox" checked={checked} onChange={handleChange}/>
-                <p>Response: {response ? 'Hello, User' : 'Goodbye, User'}</p>
-                <button onClick={handleResponse} className="bg-amber-400 rounded px-[15px] py-[4px] ">Click me!</button>
+            <div className="flex flex-col justify-center items-center gap-2 my-10">
+                <p>What do you know?</p>
+                <div>
+                    <input className="border-[1px] border-[#000000]" type="checkbox" checked={checked1} onChange={handleChange1}/>
+                    <span className="ml-2">HTML</span>
+                </div>
+                <div>
+                    <input className="border-[1px] border-[#000000]" type="checkbox" checked={checked2} onChange={handleChange2}/>
+                    <span className="ml-2">CSS</span>
+                </div>
+                <div>
+                    <input className="border-[1px] border-[#000000]" type="checkbox" checked={checked3} onChange={handleChange3}/>
+                    <span className="ml-2">JS</span>
+                </div>
+                <p>I work with: {checked1 ? 'HTML ' : ''} {checked2 ? 'CSS ' : ''} {checked3 ? 'JS' : ''}</p>
             </div>
         </>
     );
