2a02a2a3c10843649b8bdc5d59943206e17b75a9
Anastasia H. <anastasia.hrynkevich@outlook.com>
2024-05-19 18:56:25 +0100

    practise: can set defaultValue or defaultChecked, used defaultChecked

--- src/App.js
+++ src/App.js
@@ -1,44 +1,12 @@
 import React, {useState} from 'react';
 
 function App() {
-    const [currentRadioValue, setCurrentRadioValue] = useState("HTML");
-
-    function handleChange(event) {
-        setCurrentRadioValue(event.target.value);
-    }
+    const [checked, setChecked] = useState(true);
 
     return (
         <>
-            <div className="flex flex-col items-center justify-center my-10">
-                <p>What do you know?</p>
-                <div>
-                    <input
-                        onChange={handleChange}
-                        value="HTML"
-                        checked={currentRadioValue === "HTML"}
-                        type="radio"
-                        name="radio"/>
-                    <span className="ml-2">HTML</span>
-                </div>
-                <div>
-                    <input
-                        onChange={handleChange}
-                        value="CSS"
-                        checked={currentRadioValue === "CSS"}
-                        type="radio"
-                        name="radio"/>
-                    <span className="ml-2">CSS</span>
-                </div>
-                <div>
-                    <input
-                        onChange={handleChange}
-                        value="JS - Well done!"
-                        checked={currentRadioValue === "JS - Well done!"}
-                        type="radio"
-                        name="radio"/>
-                    <span className="ml-2">JS</span>
-                </div>
-                <p>I know {currentRadioValue}</p>
+            <div>
+                <input type="checkbox" defaultChecked={checked}/>
             </div>
         </>
     )
