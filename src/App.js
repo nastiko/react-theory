459c2d4c2685b2f875937bd21237f3a6c3aac4fa
Anastasia H. <anastasia.hrynkevich@outlook.com>
2024-05-19 12:13:59 +0100

    practise: working with checkbox - ex1

--- src/App.js
+++ src/App.js
@@ -1,41 +1,23 @@
 import React, {useState} from 'react';
 
 function App() {
-    const [value1, setValue1] = useState('');
-    const [value2, setValue2] = useState('');
+    const [checked, setChecked] = useState(true);
+    const [response, setResponse] = useState(true);
 
-    const [sum, setSum] = useState(0);
-    const [multiply, setMultiply] = useState(0);
-
-    function handleChange1 (event) {
-        return setValue1(event.target.value);
+    function handleChange() {
+        setChecked(!checked);
     }
 
-    function handleChange2 (event) {
-        return setValue2(event.target.value);
-    }
-
-    function handleSum() {
-        return setSum(Number(value1) + Number(value2));
-    }
-
-    function handleMultiply() {
-        return setMultiply(Number(value1) * Number(value2));
+    function handleResponse() {
+        setResponse(checked);
     }
 
     return (
         <>
-            <div className="flex gap-2">
-                <input className="border-[1px] border-[#000000]" type="number" onChange={handleChange1}/>
-                <input className="border-[1px] border-[#000000]" type="number" onChange={handleChange2}/>
-            </div>
-            <div className="flex gap-2 mt-6">
-                <p>Find sum: {sum}</p>
-                <p>Find multiply: {multiply}</p>
-            </div>
-            <div className="flex gap-2 mt-6">
-                <button onClick={handleSum} className="bg-orange-500 rounded px-[15px] py-[4px] ">Sum</button>
-                <button onClick={handleMultiply} className="bg-amber-400 rounded px-[15px] py-[4px] ">Multiply</button>
+            <div className="flex justify-center items-center gap-2 my-10">
+                <input className="border-[1px] border-[#000000]" type="checkbox" checked={checked} onChange={handleChange}/>
+                <p>Response: {response ? 'Hello, User' : 'Goodbye, User'}</p>
+                <button onClick={handleResponse} className="bg-amber-400 rounded px-[15px] py-[4px] ">Click me!</button>
             </div>
         </>
     );
