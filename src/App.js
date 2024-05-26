b8fa36b2cf5cca58f2a12f6b2feac681935becc6
Anastasia H. <anastasia.hrynkevich@outlook.com>
2024-05-26 11:25:22 +0100

    practise: Binding inputs to an array in React

--- src/App.js
+++ src/App.js
@@ -1,13 +1,38 @@
 import React, {useState} from 'react';
 
 function App() {
-    const [checked, setChecked] = useState(true);
+    const [notes, setNotes] = useState([1, 2, 3, 4, 5, 6, 7, 8, 9]);
+
+    function getMean(arr) {
+        let mean = 0;
+
+        for (let elem of arr) {
+            mean += elem;
+        }
+
+        return mean / arr.length;
+    }
+
+    function changeHandler(i, event) {
+        const value = parseInt(event.target.value);
+        setNotes([...notes.slice(0, i), value, ...notes.slice(i + 1)]);
+    }
+
+    const mean = notes.map((note, i) => {
+        return <input
+            className="border-[1px] border-[#0000000]"
+            type="number"
+            value={note}
+            key={i}
+            onChange={event => changeHandler(i, event)}/>
+    })
 
     return (
         <>
-            <div>
-                <input type="checkbox" defaultChecked={checked}/>
+            <div className="flex gap-4">
+                {mean}
             </div>
+            <p>The mean is {getMean(notes)}</p>
         </>
     )
 }
